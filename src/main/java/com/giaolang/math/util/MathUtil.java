c6e95ed6e4b41fcfb16dbec070346aa1a22dd347
AnhTranLT03 <anhtranlt03@gmail.com>
2024-06-12 16:26:51 +0700

    update readme to demonstrate the regression test

--- src/main/java/com/giaolang/math/util/MathUtil.java
+++ src/main/java/com/giaolang/math/util/MathUtil.java
@@ -9,11 +9,15 @@ package com.giaolang.math.util;
  * @author ADMIN
  */
 public class MathUtil {
+    public static void SayHiFromServer(){
+    System.out.println("Hello World!");
+}
     //this medthod is written in local
 public static void SayHiFromLocal(){
     System.out.println("Hello World!");
-    
 }
+
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
